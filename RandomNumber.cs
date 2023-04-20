bd42af36e6a315b5d553b3ff5304823b5afc2ca9
KoinGG <supermatvey100@mail.ru>
2023-04-20 17:34:49 +0700

    Создал класс RandomNumber и метод RandomNumber

---
+++ RandomNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIAPO
+{
+    public class RandomNumber
+    {
+        public static void GenerateRandomNumber()
+        {
+            Console.Write("Введите минимальное число: ");
+            int minNum = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите максимальное число: ");
+            int maxNum = int.Parse(Console.ReadLine());
+
+            Random random = new Random(); 
+            int randomNumber = random.Next(minNum, maxNum);
+
+            Console.WriteLine($"\nВаше случайное число: {randomNumber}");
+        }
+    }
+}
